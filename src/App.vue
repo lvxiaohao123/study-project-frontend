24d8614bf98b7dad19d9a858bd9a01f83a8ba96a
seashell <2911933126@qq.com>
2023-12-08 18:27:51 +0800

    接口修改

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import router from "@/router";
 const store = useStore()
 
 if(store.auth.user == null) {
-    get('/api/me', (message) => {
+    get('/api/user/me', (message) => {
         store.auth.user = message
         router.push('/index')
     }, () => {
